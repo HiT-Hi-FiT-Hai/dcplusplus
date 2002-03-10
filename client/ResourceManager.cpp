6990cab8024e65883c14191315715166734f874d
arnetheduck <arnetheduck@f586c4b0-9d0d-0410-bc51-f1fe239209fc>
2002-03-10 22:41:43 +0000

    First go at the new resource managment...
    git-svn-id: http://dcplusplus.svn.sourceforge.net/svnroot/dcplusplus/dcplusplus/trunk@129 f586c4b0-9d0d-0410-bc51-f1fe239209fc

---
+++ client/ResourceManager.cpp
@@ -0,0 +1,36 @@
+/* 
+ * Copyright (C) 2001 Jacek Sieka, j_s@telia.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include "stdafx.h"
+#include "DCPlusPlus.h"
+
+#include "ResourceManager.h"
+
+ResourceManager* ResourceManager::instance;
+
+/**
+ * @file ResourceManager.h
+ * $Id: ResourceManager.cpp,v 1.1 2002/03/10 22:41:43 arnetheduck Exp $
+ * @if LOG
+ * $Log: ResourceManager.cpp,v $
+ * Revision 1.1  2002/03/10 22:41:43  arnetheduck
+ * First go at the new resource managment...
+ *
+ * @endif
+ */
+
---
+++ client/ResourceManager.h
@@ -0,0 +1,53 @@
+/* 
+ * Copyright (C) 2001 Jacek Sieka, j_s@telia.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#if !defined(AFX_RESOURCEMANAGER_H__AA978E1D_82F9_434B_8C3C_1D58B93F7582__INCLUDED_)
+#define AFX_RESOURCEMANAGER_H__AA978E1D_82F9_434B_8C3C_1D58B93F7582__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+class ResourceManager : public Singleton<ResourceManager> 
+{
+public:
+	StringList getLanguages();
+	void loadLanguage(const string& aLanguage);
+
+	const string& getString();
+private:
+
+	friend class Singleton<ResourceManager>;
+	ResourceManager();
+	virtual ~ResourceManager();
+	
+};
+
+#endif // !defined(AFX_RESOURCEMANAGER_H__AA978E1D_82F9_434B_8C3C_1D58B93F7582__INCLUDED_)
+
+/**
+ * @file ResourceManager.h
+ * $Id: ResourceManager.h,v 1.1 2002/03/10 22:41:43 arnetheduck Exp $
+ * @if LOG
+ * $Log: ResourceManager.h,v $
+ * Revision 1.1  2002/03/10 22:41:43  arnetheduck
+ * First go at the new resource managment...
+ *
+ * @endif
+ */
+
