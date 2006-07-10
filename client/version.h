18589359d46bba37ec78e4a239a0772e7b8894ae
arnetheduck <arnetheduck@f586c4b0-9d0d-0410-bc51-f1fe239209fc>
2006-07-10 20:34:17 +0000

    0.694 fixes
    git-svn-id: http://dcplusplus.svn.sourceforge.net/svnroot/dcplusplus/dcplusplus/trunk@636 f586c4b0-9d0d-0410-bc51-f1fe239209fc

--- client/version.h
+++ client/version.h
@@ -17,8 +17,8 @@
  */
 
 #define APPNAME "DC++"
-#define VERSIONSTRING "0.693"
-#define VERSIONFLOAT 0.693
+#define VERSIONSTRING "0.694"
+#define VERSIONFLOAT 0.694
 
 /* Update the .rc file as well... */
 
