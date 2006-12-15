5fdf8150b1df80624cb870a292cf00a8cabb64fa
arnetheduck <arnetheduck@f586c4b0-9d0d-0410-bc51-f1fe239209fc>
2006-12-15 21:38:18 +0000

    exclude fix
    git-svn-id: http://dcplusplus.svn.sourceforge.net/svnroot/dcplusplus/dcplusplus/trunk@694 f586c4b0-9d0d-0410-bc51-f1fe239209fc

--- release.cmd
+++ release.cmd
@@ -2,7 +2,7 @@ copy /b app\dcplusplus.exe .
 copy /b app\dcplusplus.chm .
 copy /b app\dcplusplus.pdb .
 "C:\Program Files\7-Zip\7z.exe" a -tzip -mx9 DCPlusPlus-%1.zip dcppboot.xml unicows.dll unicows.pdb dcplusplus.chm dcplusplus.exe dcplusplus.pdb dbghelp.dll changelog.txt Example.xml License.txt GeoIPCountryWhois.csv
-"C:\Program Files\7-Zip\7z.exe" a -tzip -mx9 DCPlusPlus-%1-src.zip dcppboot.xml makedefs.py libunicows.lib unicows.dll unicows.pdb yassl\* yassl\certs\* yassl\include\openssl\* yassl\mySTL\* yassl\src\* yassl\include\* yassl\taocrypt\* yassl\taocrypt\src\* yassl\taocrypt\include\* stlport\.keep wtl\.keep help\* zlib\* bzip2\* client\* res\* windows\* changelog.txt Example.xml License.txt compile.txt GeoIPCountryWhois.csv *.vcproj *.sln *.rc extensions.txt doxyfile dcplusplus.nsi -xr!.svn -x!*.user
+"C:\Program Files\7-Zip\7z.exe" a -tzip -mx9 DCPlusPlus-%1-src.zip dcppboot.xml makedefs.py libunicows.lib unicows.dll unicows.pdb yassl\* yassl\certs\* yassl\include\openssl\* yassl\mySTL\* yassl\src\* yassl\include\* yassl\taocrypt\* yassl\taocrypt\src\* yassl\taocrypt\include\* stlport\.keep wtl\.keep help\* zlib\* bzip2\* client\* res\* windows\* changelog.txt Example.xml License.txt compile.txt GeoIPCountryWhois.csv *.vcproj *.sln *.rc extensions.txt doxyfile dcplusplus.nsi -xr!.svn -xr!*.user
 del dcplusplus.exe
 del dcplusplus.pdb
 del dcplusplus.chm
