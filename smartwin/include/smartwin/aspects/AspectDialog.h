57308c54af4604db5e6bd82ca809f261c94b2853
arnetheduck <arnetheduck@f586c4b0-9d0d-0410-bc51-f1fe239209fc>
2007-07-08 09:34:08 +0000

    missing file
    git-svn-id: http://dcplusplus.svn.sourceforge.net/svnroot/dcplusplus/dcplusplus/trunk@788 f586c4b0-9d0d-0410-bc51-f1fe239209fc

---
+++ smartwin/include/smartwin/aspects/AspectDialog.h
@@ -0,0 +1,15 @@
+#ifndef ASPECTDIALOG_H_
+#define ASPECTDIALOG_H_
+
+namespace SmartWin {
+
+template<typename T>
+class AspectDialog {
+public:
+	void setItemText(int id, const SmartUtil::tstring& text) {
+		::SetDlgItemText(static_cast<T*>(this)->handle(), id, text.c_str());
+	}
+	
+};
+}
+#endif /*ASPECTDIALOG_H_*/
