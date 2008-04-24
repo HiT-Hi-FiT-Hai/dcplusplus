8d569ba4daf1db58bf8402f1260b0af3c49f571e
arnetheduck <arnetheduck@f586c4b0-9d0d-0410-bc51-f1fe239209fc>
2008-04-24 13:17:49 +0000

    Fixed PM history not showing the last line
    git-svn-id: http://dcplusplus.svn.sourceforge.net/svnroot/dcplusplus/dcplusplus/trunk@1187 f586c4b0-9d0d-0410-bc51-f1fe239209fc

--- win32/PrivateFrame.cpp
+++ win32/PrivateFrame.cpp
@@ -178,39 +178,39 @@ bool PrivateFrame::preClosing() {
 }
 
 void PrivateFrame::readLog() {
-	StringMap params;
+	if(SETTING(SHOW_LAST_LINES_LOG) == 0)
+		return;
 
+	StringMap params;
 	params["hubNI"] = Util::toString(ClientManager::getInstance()->getHubNames(replyTo->getCID()));
 	params["hubURL"] = Util::toString(ClientManager::getInstance()->getHubs(replyTo->getCID()));
 	params["userCID"] = replyTo->getCID().toBase32();
 	params["userNI"] = ClientManager::getInstance()->getNicks(replyTo->getCID())[0];
 	params["myCID"] = ClientManager::getInstance()->getMe()->getCID().toBase32();
-
 	string path = Util::validateFileName(SETTING(LOG_DIRECTORY) + Util::formatParams(SETTING(LOG_FILE_PRIVATE_CHAT), params, true));
 
+	StringList lines;
+
 	try {
-		if (SETTING(SHOW_LAST_LINES_LOG) > 0) {
-			File f(path, File::READ, File::OPEN);
-
-			int64_t size = f.getSize();
-
-			if(size > 32*1024) {
-				f.setPos(size - 32*1024);
-			}
-
-			StringList lines = StringTokenizer<string>(f.read(32*1024), "\r\n").getTokens();
-
-			int linesCount = lines.size();
-
-			int i = linesCount > (SETTING(SHOW_LAST_LINES_LOG) + 1) ? linesCount - (SETTING(SHOW_LAST_LINES_LOG) + 1) : 0;
-
-			for(; i < (linesCount - 1); ++i){
-				addStatus(_T("- ") + Text::toT(lines[i]));
-			}
-
-			f.close();
+		File f(path, File::READ, File::OPEN);
+		if(f.getSize() > 32*1024) {
+			f.setEndPos(32*1024);
 		}
-	} catch(const FileException&){
+
+		lines = StringTokenizer<string>(f.read(32*1024), "\r\n").getTokens();
+
+		f.close();
+	} catch(const FileException&) { }
+
+	if(lines.empty())
+		return;
+
+	// the last line in the log file is an empty line; remove it
+	lines.pop_back();
+
+	size_t linesCount = lines.size();
+	for(size_t i = std::max(static_cast<int>(linesCount) - SETTING(SHOW_LAST_LINES_LOG), 0); i < linesCount; ++i) {
+		addStatus(_T("- ") + Text::toT(lines[i]));
 	}
 }
 
